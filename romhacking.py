5c7efada76ca49f14dc31ba535451c6664adc636
Cody Garver <codygarver@gmail.com>
2022-04-17 14:34:53 -0400

    romhacking: add --tests and sha1 support

--- romhacking.py
+++ romhacking.py
@@ -20,6 +20,19 @@ def get_romhacking(url):
 
     soup = BeautifulSoup(html.text, "html.parser")
 
+    rom_info_div = soup.find(
+        "div", {"id": "rom_info"})
+
+    list_items = rom_info_div.find_all("li")
+
+    sha1 = ""
+    sha1_line_regex = "^(FILE\/ROM\sSHA\-1\:.*|.*ROM\sSHA\-1\:.*|SHA\-1\:.*|.*SHA\-1.*|SHA\s1)"
+    sha1_sum_regex = "[a-f0-9]{40}"
+    for item in list_items:
+        if re.search(sha1_line_regex, item.text):
+            sha1 = re.search(sha1_sum_regex, item.text.lower()).group()
+            break
+
     table_body = soup.find(
         "table", {"class": "entryinfo entryinfosmall"})
 
@@ -40,7 +53,7 @@ def get_romhacking(url):
     platform = rom_info[3].lower()
     version = rom_info[7]
 
-    return name, platform, version
+    return name, platform, version, sha1
 
 
 def sigint_handler(signal, frame):
@@ -55,13 +68,13 @@ def add(hack):
         id = re.search(r'\d+', hack).group()
         url = hack
 
-    name, platform, version = get_romhacking(url)
+    name, platform, version, sha1 = get_romhacking(url)
 
     patches_dict.update({name: {
         "filename": "",
         "id": id,
         "platform": platform,
-        "sha1": "",
+        "sha1": sha1.upper(),
         "version": version
     }})
 
@@ -81,9 +94,8 @@ def update():
 
     for patch in patches_dict:
         local = patches_dict[patch]["version"]
-        url = "https://www.romhacking.net/hacks/" + \
-            patches_dict[patch]["id"]
-        _, _, latest = get_romhacking(url)
+        url = "https://www.romhacking.net/hacks/" + patches_dict[patch]["id"]
+        _, _, latest, _ = get_romhacking(url)
         if local != latest:
             if args.update_github:
                 github(patch, local, latest, url)
@@ -115,6 +127,23 @@ def github(romhack_name, local_version, latest_version, url):
               (romhack_name, latest_version, local_version, url, issue.number))
 
 
+def tests():
+    if not patches_dict:
+        print("Empty config")
+        exit(1)
+
+    fail = False
+
+    for patch in patches_dict:
+        for key in patches_dict[patch]:
+            if not patches_dict[patch][key]:
+                print("Missing " + key + ": " + patch)
+                fail = True
+
+    if fail:
+        exit(1)
+
+
 signal.signal(signal.SIGINT, sigint_handler)
 
 if __name__ == "__main__":
@@ -123,16 +152,18 @@ if __name__ == "__main__":
     description = "Manage rom hacks and check for updates"
     parser = argparse.ArgumentParser(
         description=description)
-    parser.add_argument("--add")
-    parser.add_argument("--config")
+    parser.add_argument("--config", required=True)
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--add")
+    group.add_argument("--tests", action='store_true')
+    group.add_argument("--update", action='store_true')
+    group.add_argument("--update-github", action='store_true')
     parser.add_argument("--debug", action='store_true')
-    parser.add_argument("--update", action='store_true')
-    parser.add_argument("--update-github", action='store_true')
 
     args = parser.parse_args()
 
     # Read json to dictionary
-    if os.path.exists(args.config):
+    if os.path.exists(str(args.config)):
         patches_file = open(args.config)
         patches_dict = json.load(patches_file)
     else:
@@ -140,6 +171,9 @@ if __name__ == "__main__":
 
     if args.add:
         add(args.add)
+
+    if args.tests:
+        tests()
 
     if args.update or args.update_github:
         update()
